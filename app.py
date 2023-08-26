e3c16be4a46cc9d4c5af56edd51e2c75a54e11c7
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-26 23:52:56 +0530

    updated

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import string
 import random
 import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
 
 MONGO_URI = os.getenv('MONGO_URI', '')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'urlshortenerdb')
